6e7c83c17295e80c16aa884ae7c2b2d78edc72fa
maxmelnit <65923935+JryMx@users.noreply.github.com>
2026-04-15 14:48:42 -0600

    Completed mailbox Gmail integration using imap

--- mailing/fetch_tickets.py
+++ mailing/fetch_tickets.py
@@ -14,13 +14,23 @@ def request_mail():
     mail_addr = os.getenv("EMAIL")
     mail_pass = os.getenv("PASS")
 
+    print(repr(os.getenv("MAIL_HOST")))
+    
     # Login with credentials
     inbox = imaplib.IMAP4_SSL(server_host, server_port)
     inbox.login(mail_addr, mail_pass)
-    inbox.select("INBOX")
+    print("Logged in successfully")
 
     # Check the status of the inbox, exit if not OK
-    status, data = inbox.search(None)
+    status, data = inbox.select('"cpsc"')
+    print(f"Select status: {status}, data: {data}")
+    if status != "OK":
+        print("Could not find specified inbox.")
+        inbox.logout()
+        return []
+
+    status, data = inbox.search(None, "UNSEEN")
+    print(f"Search status: {status}, email IDs found: {data}")
     if status != "OK":
         inbox.logout()
         return []
@@ -45,14 +55,35 @@ def request_mail():
     return messages
 
 
-if __name__ == "__main__":
+import time
+
+def start_email_fetcher():
+    from tickets.models import Ticket
+
+    print(f"Fetching tickets every {FREQ} seconds")
+
     while True:
-        emails = request_mail()
-        for msg in emails:
-            print("Subject:", msg.get("Subject"))
-            print("From:", msg.get("From"))
-            print()
+        try:
+            emails = request_mail()
+        
+            for msg in emails:
+
+                # Get subject/body of the email
+                subject = msg.get("Subject", "No Subject")
+                body_part = msg.get_body(preferencelist=('plain',))
+
+                if body_part:
+                    body = body_part.get_content()
+                else:
+                    body = "No text content found."
+     
+                # Making a new ticket will fire off the signal in tickets/signals
+                Ticket.objects.create(
+                    subject=subject[:50], 
+                    message_body=body
+                )
+
+        except Exception as e:
+            print("An error occurred while fetching emails:", e)
 
         time.sleep(FREQ)
-
-
--- tickets/apps.py
+++ tickets/apps.py
@@ -8,3 +8,12 @@ class TicketsConfig(AppConfig):
     # Needed for the signal to work
     def ready(self):
         import tickets.signals 
+        
+        # The tickets are gonna be updated from the inbox using a background threat
+        import os
+        if os.environ.get('RUN_MAIN') == 'true':
+            
+            import threading
+            from mailing.fetch_tickets import start_email_fetcher
+            fetch_thread = threading.Thread(target=start_email_fetcher, daemon=True)
+            fetch_thread.start()
